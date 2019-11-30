b47d061267340ee5285ce3a97966285d89f2d17f
sajib1066 <sajib1066@gmail.com>
2019-11-30 21:23:43 +0600

    include cart url

--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('contact/', include('contact.urls')),
     path('about/', include('about.urls')),
-    path('shop/', include('shop.urls'))
+    path('shop/', include('shop.urls')),
+    path('cart/', include('cart.urls'))
 ]
