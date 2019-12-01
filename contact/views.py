782672eba9e148f2414936725061a726c086e17d
sajib1066 <sajib1066@gmail.com>
2019-12-01 16:44:05 +0600

    save data from from

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 from .forms import ContactForm
 
 # Create your views here.
 def contact_page(request):
     form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.INFO, 'Submitted.')
+            return redirect('contact')
     context = {
         'form': form
     }
