5f8a213c652809a993a38ba7f76197f3ea33d368
sajib1066 <sajib1066@gmail.com>
2019-12-01 17:04:44 +0600

    customize admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Contact
-# Register your models here.
-admin.site.register(Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'subject', 'date']
+
+admin.site.register(Contact, ContactAdmin)
