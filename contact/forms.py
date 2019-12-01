34bbe7a98aeb2bfaad78378a6e9a15a76505f3f8
sajib1066 <sajib1066@gmail.com>
2019-12-01 16:27:16 +0600

    create contact form

---
+++ contact/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Message'})
+        }
