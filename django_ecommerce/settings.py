79269dd4301dd226a0d3382a381681ea22c4e0cf
sajib1066 <sajib1066@gmail.com>
2019-12-03 19:13:49 +0600

    add media settings

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from . import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +29,6 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('cart/', include('cart.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
