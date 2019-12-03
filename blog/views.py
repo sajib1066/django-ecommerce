be0a70d57f118fed90faa557be507513c027fc68
sajib1066 <sajib1066@gmail.com>
2019-12-03 20:26:30 +0600

    add post count in category list

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.db.models import Count
+
 from .models import Category, Tag, Post
 
 def blog_page(request):
     post = Post.objects.filter(is_draft=False)
-    category = Category.objects.all()
+    category = Category.objects.all().annotate(number_of_posts=Count('post')).order_by('-number_of_posts', 'name')
     tag = Tag.objects.all()
     recent_post = Post.objects.filter(is_draft=False).order_by('-date')[:3]
     context = {
