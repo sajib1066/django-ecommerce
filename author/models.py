b800bda862fe44d678380805702395331af7ac09
sajib1066 <sajib1066@gmail.com>
2019-12-03 19:01:58 +0600

    create author profile

--- author/models.py
+++ author/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class AuthorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    about = models.TextField()
+    photo = models.ImageField(upload_to='author')
+    gender_choice = (
+        ('male', 'Male'),
+        ('female', 'Female')
+    )
+    gender = models.CharField(choices=gender_choice, max_length=6)
+    website = models.URLField(blank=True, null=True)
+    facebook = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
