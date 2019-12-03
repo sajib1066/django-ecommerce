c34e5f055af3b499f358690dd33f471197f05244
sajib1066 <sajib1066@gmail.com>
2019-12-03 19:45:26 +0600

    customize blog admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Tag, Post
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'date']
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'date']
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'author', 'is_draft', 'date']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
