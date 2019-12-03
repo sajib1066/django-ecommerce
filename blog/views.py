8a0a8cf6936ca9f96d673c7bd3149b42e6d41a86
sajib1066 <sajib1066@gmail.com>
2019-12-03 20:17:20 +0600

    show post, category, tags in blog page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Category, Tag, Post
 
 def blog_page(request):
-    return render(request, 'blog/blog.html')
+    post = Post.objects.filter(is_draft=False)
+    category = Category.objects.all()
+    tag = Tag.objects.all()
+    recent_post = Post.objects.filter(is_draft=False).order_by('-date')[:3]
+    context = {
+        'posts': post,
+        'categories': category,
+        'tags': tag,
+        'recent_posts': recent_post
+    }
+    return render(request, 'blog/blog.html', context)
