f4dfc544486f44ddd98dba3611800b99f12d5141
sajib1066 <sajib1066@gmail.com>
2019-12-03 19:40:49 +0600

    create models for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+from author.models import AuthorProfile
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='post')
+    content = RichTextUploadingField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    author = models.ForeignKey(AuthorProfile, on_delete=models.CASCADE)
+    is_draft = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
