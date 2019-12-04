cc524ab4157462353c142b8093a8669dd46be206
sajib1066 <sajib1066@gmail.com>
2019-12-04 10:53:46 +0600

    create post details page url

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_page, name='blog')
+    path('', views.blog_page, name='blog'),
+    path('details/<post_id>', views.post_details, name='post-details')
 ]
