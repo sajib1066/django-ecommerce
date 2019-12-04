0934e355dc79239e80fbe70de2202f7b2c259c9c
sajib1066 <sajib1066@gmail.com>
2019-12-04 12:15:34 +0600

    create products

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from author.models import AuthorProfile
+from blog.models import Category
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='products')
+    price = models.IntegerField()
+    details = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(AuthorProfile, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
