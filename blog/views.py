aef9e8eba83b92e436e8b8bd7a3956ef4f68b97c
sajib1066 <sajib1066@gmail.com>
2019-12-04 11:58:28 +0600

    update views.py

--- blog/views.py
+++ blog/views.py
@@ -18,7 +18,13 @@ def blog_page(request):
 
 def post_details(request, post_id):
     post = Post.objects.get(id=post_id)
+    category = Category.objects.all().annotate(number_of_posts=Count('post')).order_by('-number_of_posts', 'name')
+    tag = Tag.objects.all()
+    recent_post = Post.objects.filter(is_draft=False).order_by('-date')[:3]
     context = {
-        'post': post
+        'post': post,
+        'categories': category,
+        'tags': tag,
+        'recent_posts': recent_post
     }
     return render(request, 'blog/post-details.html', context)
