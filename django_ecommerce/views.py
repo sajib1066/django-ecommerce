42223d435827d708f8125fe1cf0bf98202fe93b1
sajib1066 <sajib1066@gmail.com>
2019-12-05 23:08:53 +0600

    show product list in home page

--- django_ecommerce/views.py
+++ django_ecommerce/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from shop.models import Product
 
 def home_page(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()[:8]
+    context = {
+        'products': products
+    }
+    return render(request, 'home.html', context)
