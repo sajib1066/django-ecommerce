fd5707aa6bc3b8b4ed712096660ab3aaa944b622
sajib1066 <sajib1066@gmail.com>
2019-12-06 21:53:16 +0600

    create product category model

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from author.models import AuthorProfile
-from blog.models import Category
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    photo = models.ImageField(upload_to='products_category')
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
