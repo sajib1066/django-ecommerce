8606b470ea4981ded3436148aeca70be96f470b4
sajib1066 <sajib1066@gmail.com>
2019-12-08 20:38:31 +0600

    setup chechout page

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart_page, name='cart')
+    path('', views.cart_page, name='cart'),
+    path('checkout', views.checkout, name='checkout')
 ]
