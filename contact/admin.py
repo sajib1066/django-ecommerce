8e3059d7bc312e78ec0bbc262c95236f6d918b06
sajib1066 <sajib1066@gmail.com>
2019-12-13 23:12:56 +0600

    add subscriber model to admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, Subscriber
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'subject', 'date']
 
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Subscriber)
