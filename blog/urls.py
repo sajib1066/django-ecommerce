cde4a96d76f1d7fe21ca2b49665119d8ceb7700d
sajib1066 <sajib1066@gmail.com>
2019-12-16 21:35:52 +0600

    create post by category views & url

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_page, name='blog'),
-    path('details/<post_id>', views.post_details, name='post-details')
+    path('details/<post_id>', views.post_details, name='post-details'),
+    path('category/<ctg_name>', views.post_by_category, name='post-by-category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -28,3 +28,17 @@ def post_details(request, post_id):
         'recent_posts': recent_post
     }
     return render(request, 'blog/post-details.html', context)
+
+def post_by_category(request, ctg_name):
+    ctg = Category.objects.get(name=ctg_name)
+    post = Post.objects.filter(category=ctg)
+    category = Category.objects.all().annotate(number_of_posts=Count('post')).order_by('-number_of_posts', 'name')
+    tag = Tag.objects.all()
+    recent_post = Post.objects.filter(is_draft=False).order_by('-date')[:3]
+    context = {
+        'posts': post,
+        'categories': category,
+        'tags': tag,
+        'recent_posts': recent_post
+    }
+    return render(request, 'blog/post-by-category.html', context)
