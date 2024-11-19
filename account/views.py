5b0561178222af7648e03547d296ddabbd58c109
guru1998-ps <kollapudi1998@gmail.com>
2024-11-19 13:12:52 +0530

    adding

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import  MyUserCreationForm
-
+#define
 def signup(request):
     if request.user.is_authenticated:
         return redirect('/')
